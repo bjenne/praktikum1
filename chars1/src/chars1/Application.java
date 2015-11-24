d82fb0fd7abcd0a32c115c1d41d12f7b85673766
bjenne <bjenne@philnet-pc0041.philnet.ad.uni-koeln.de>
2015-11-24 11:02:18 +0100

    Application from CharChecker

---
+++ chars1/src/chars1/Application.java
@@ -0,0 +1,15 @@
+package chars1;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		CharChecker cc = new CharChecker();
+		String toCheck = "fsfsfsf";
+		
+		cc.iterate(toCheck);
+	}
+
+}
