86fc683ba13467dd1f67166899034a65a27ad406
bjenne <bjenne@philnet-pc0041.philnet.ad.uni-koeln.de>
2015-12-01 11:44:49 +0100

    charChecker

--- chars1/src/chars1/CharChecker.java
+++ chars1/src/chars1/CharChecker.java
@@ -2,50 +2,44 @@ package chars1;
 
 public class CharChecker {
 
-	
-void iterate (String s){
-		for (int i = 0; i < s.length(); i++ ){
-			
+	void iterate(String s) {
+		for (int i = 0; i < s.length(); i++) {
+
 			char current = s.charAt(i);
-			System.out.println(current + "\t" + isLowerCase(current) + "\t" + decValue (current));
+			System.out.println(current + "\t" + isLowerCase(current) + "\t" + decValue(current));
+		}
+	}
+
+	boolean isLowerCase(char current) {
+		if (current >= 'a' && current <= 'z') {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	int decValue(char current) {
+		int i = current;
+		return i;
+	}
+
+	char[] toArray(String s) {
+		char[] charArray = new char[s.length()];
+
+		for (int i = 0; i < s.length();i++) {
+
+			charArray[i] = s.charAt(i);
+		}
+		return charArray;
+	}
+
+	void printArray(char[] charArray) {
+		int current = 0;
+		int breakAt = charArray.length;
+
+		while (current < breakAt) {
+			System.out.println("position " + current + "- value: " + charArray[current]);
+			current++;
+		}
 	}
 }
-
-		boolean isLowerCase(char current){
-		if (current >= 'a' && current <= 'z'){
-		return true;
-	}	else {
-		return false;
-	}
-	}
-		
-		int decValue (char current){
-			int i = current;
-			return i;
-		}
-		
-		
-		
-		char [] toArray(String s){
-	 char [] charArray = new char[s.length()];
-	
-	 for (int i = 0; i<s.length();){
-	
-		 charArray[i] = s.charAt(i);
-	 }
-	 	return charArray;
-		}
-		
-		void printArray(char [] charArray){
-			int current = 0;
-			int breakAt = charArray.length;
-			
-		while(current<breakAt){
-		System.out.println("position " + current+ "- value: "+ charArray[current]);
-		current++;
-		}
-		}
-}
-
-
-
