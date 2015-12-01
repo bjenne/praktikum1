d6a8d71676171bf98828580a871d23ded0c81d79
bjenne <bjenne@philnet-pc0041.philnet.ad.uni-koeln.de>
2015-12-01 11:13:52 +0100

    Chars

--- chars1/src/chars1/CharChecker.java
+++ chars1/src/chars1/CharChecker.java
@@ -3,14 +3,13 @@ package chars1;
 public class CharChecker {
 
 	
-void iterate (String s){
-		
-		for (int i = 0; i < s.length(); i++ ){
+//void iterate (String s){
+		//for (int i = 0; i < s.length(); i++ ){
 			
-			char current = s.charAt(i);
-			System.out.println(current + "\t" + isLowerCase(current) + "\t" + decValue (current));
-		}
-	}
+			//char current = s.charAt(i);
+			//System.out.println(current + "\t" + isLowerCase(current) + "\t" + decValue (current));
+	//}
+//}
 
 		boolean isLowerCase(char current){
 		if (current >= 'a' && current <= 'z'){
@@ -24,5 +23,24 @@ void iterate (String s){
 			int i = current;
 			return i;
 		}
+		
+		char [] toArray(String s){
+	 char [] charArray = new char[s.length()];
+	
+	 for (int i = 0; i<s.length();){
+	
+		 charArray[i] = s.charAt(i);
+	 }
+	 	return charArray;
+		}
 }
+		//char[] toArray(String s)
+		//add every char of a String into a char array
+		//use a for loop
+
+//void printArray(char [] toPrint) 
+//length of Array
+	//iterate over every element of the array while printing its position in the array and its value
+	//use a while loop
+
 
