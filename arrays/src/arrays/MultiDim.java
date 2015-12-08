434224af86d4084ceceff9d6a99ca435ae4fb0c2
bjenne <bjenne@philnet-pc0041.philnet.ad.uni-koeln.de>
2015-12-08 11:10:06 +0100

    MultiDim

--- arrays/src/arrays/MultiDim.java
+++ arrays/src/arrays/MultiDim.java
@@ -4,12 +4,30 @@ public class MultiDim {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int[][] get2D(int rows, int columns){
+	}
+		public int[][] get2D(int rows, int columns) {
 			
+			int twoDimensional [][] = new int [rows][columns];
+			
+			for (int i = 0; i<rows; i++){
+			
+			for (int j = 0; j < columns; j++){
+				
+				twoDimensional [i][j] = i *j;
 			}
+			}
+			return twoDimensional;
+		}
 		
+		public void print2D (int [][] twoDimensional){
+			
+			for (int i = 0; i < twoDimensional.length; i++) {
+				for (int j = 0; j < twoDimensional [i].length; j++){
+					System.out.println(twoDimensional[i][j] + "\t");
+				}
+				System.out.println();
+		}
 
 	}
-
 }
+
