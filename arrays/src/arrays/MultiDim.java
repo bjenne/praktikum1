9f88f891838c33d514051c14176ba5afda71896d
bjenne <bjenne@philnet-pc0041.philnet.ad.uni-koeln.de>
2015-12-08 10:57:53 +0100

    arrays

---
+++ arrays/src/arrays/MultiDim.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class MultiDim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[][] get2D(int rows, int columns){
+			
+			}
+		
+
+	}
+
+}
